9e0d9e0fc9ce9fc624299ebce0e369877798edf9
TLC <tituslifecharlie@gmail.com>
2024-11-06 20:20:56 +0100

    Add index.html, style.css, script.js, and questions.js

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyperlinks and Text Markup Language",
+            "B. Home Tool Markup Language",
+            "C. Hyper Text Markup Language",
+            "D. Hyperlinking Text Marketing Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheets",
+        options: [
+            "A. Cascading Style Sheets",
+            "B. Creative Style Sheets",
+            "C. Computer Style Sheets",
+            "D. Colorful Style System"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "D. PHP: Hypertext Preprocessor",
+        options: [
+            "A. Private Home Page",
+            "B. Personal Hypertext Processor",
+            "C. Programming Hypertext Processor",
+            "D. PHP: Hypertext Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: " B. Structured Query Language",
+        options: [
+            "A. Standard Query Language",
+            "B. Structured Query Language",
+            "C. Sequential Query Language",
+            "D. Simple Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: " What does XML stand for?",
+        answer: "A. eXtensible Markup Language",
+        options: [
+            "A. eXtensible Markup Language",
+            "B. Executable Markup Language",
+            "C. Extra Multi-purpose Language",
+            "D. Example Markup Language"
+        ]
+    },
+];
+
---
+++ script.js
@@ -0,0 +1,174 @@
+const startBtn = document.querySelector('.start-btn'); 
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryagain-btn');
+const goHomeBtn = document.querySelector('.gohome-btn');
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+};
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+};
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+    
+    showQuestions(0);
+    questionCounter(1);
+    headerscore()
+}
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+     questionCount = 0;
+     questionNumb = 1;
+     userScore = 0;
+     showQuestions(questionCount);
+     questionCounter(questionNumb);
+
+     headerscore();
+};
+
+goHomeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+     questionCount = 0;
+     questionNumb = 1;
+     userScore = 0;
+     showQuestions(questionCount);
+     questionCounter(questionNumb);
+};
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++; 
+        showQuestions(questionCount);
+
+        questionNumb++; 
+        questionCounter(questionNumb); 
+
+        nextBtn.classList.remove('active');
+
+
+    } else {
+        showResultBox();
+    }
+};
+
+ 
+ const optionList = document.querySelector('.option-list');
+
+// getting questions and answers from array
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`; 
+
+    let optionTag = `
+    <div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const options = document.querySelectorAll('.option');
+    for (let i = 0; i < options.length; i++) {
+    options[i].setAttribute('onclick', 'optionSelected(this)');
+  }
+
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length; 
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+        userScore += 1;
+        headerscore()
+    }
+    else {
+        answer.classList.add('incorrect');
+
+         // if answer is incorrect auto select correct answer
+         for (let i = 0; i < allOptions; i++) { 
+            if (optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+
+        }
+    }
+
+    // if user has selected disable all options
+    for (let i = 0; i < allOptions; i++) { 
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+    
+}
+
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerscore() {
+    const headerscoreText = document.querySelector('.header-score');
+    headerscoreText.textContent = `score ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active'); 
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your score ${userScore} out of ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    
+    let progress = setInterval(() => {
+         progressStartValue++;
+        
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, 0.1) 0deg)`;
+
+
+         if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+
+    }, speed)
+}
+
+
+
